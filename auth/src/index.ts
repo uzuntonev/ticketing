8a1bfb9186830f919b8416e9295c88862d5d2f9e
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2021-03-10 19:35:55 +0200

    Add error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
-import { currentUserRouter } from './routes/current-user'
-import { signInRouter } from './routes/signin'
-import { signOutRouter } from './routes/signout'
-import { signUpRouter } from './routes/signup'
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const PORT = 3000;
 const app = express();
@@ -15,6 +18,13 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError
+});
+
+app.use(errorHandler);
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
