96fae0c5331085fb8000db5bd7475e1d25fee353
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2021-03-19 15:00:35 +0200

    Add authentication

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
 import { UserModel } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionValidationError } from '../errors/database-connection-error';
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
@@ -16,12 +17,8 @@ router.post('/api/users/signup', [
     .isLength({ min: 4, max: 20 })
     .withMessage('Password must be between 4 and 20 characters')
 ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array())
-    }
 
     const { email, password } = req.body;
 
@@ -33,6 +30,15 @@ router.post('/api/users/signup', [
     const user = UserModel.build({ email, password });
 
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, process.env.JWT_KEY!);
+
+    // Store it on session object
+    req.session = { jwt: userJwt };
 
     res.status(201).send(user)
   })
