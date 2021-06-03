a2560c226b5323cf7b199c4bd5ae66922f9cd4a3
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2021-06-03 15:10:01 +0300

    Init payments service

---
+++ payments/src/models/order.ts
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@gutickets/common';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+interface OrderAttrs {
+  id: string,
+  status: OrderStatus,
+  version: number,
+  userId: string,
+  price: number,
+}
+interface OrderDoc extends mongoose.Document {
+  status: OrderStatus,
+  version: number,
+  userId: string,
+  price: number,
+}
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc
+}
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: Object.values(OrderStatus),
+  },
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id
+      delete ret._id
+    }
+  }
+});
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    userId: attrs.userId,
+    status: attrs.status,
+    version: attrs.version,
+    price: attrs.price
+  })
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order, OrderStatus };
