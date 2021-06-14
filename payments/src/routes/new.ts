9638111433527641ab1311e97d9fabf16e93feb4
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2021-06-14 20:12:04 +0300

    Create payment:created publisher/listener

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -11,6 +11,8 @@ import {
 import { natsWrapper } from '../nats-wrapper';
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
+import { Payment } from '../models/payment';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
 
 const router = express.Router();
 
@@ -40,13 +42,25 @@ router.post('/api/payments', requireAuth, [
       throw new BadRequestError('Cannot pay for an cancelled order')
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: 'usd',
       amount: order.price * 100,
       source: token
     });
 
-    res.status(201).send({ success: true });
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id
+    });
+    await payment.save();
+
+    await new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId
+    })
+
+    res.status(201).send({ id: payment.id });
   })
 
 export { router as createChargeRouter }
