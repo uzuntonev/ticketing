59fd085a985d4626e304ce716fa92474d3274e1f
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2021-08-03 20:30:32 +0300

    added startup message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,6 +9,8 @@ import { natsWrapper } from './nats-wrapper';
 const PORT = 3000;
 
 const start = async () => {
+  console.log("Starting up...");
+  
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined')
   }
